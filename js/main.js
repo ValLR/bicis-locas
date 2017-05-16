6dee406913fe34662b1416a2cd4696a179cf4fe2
valentina lopez <valentina lopez>
2017-05-16 06:44:14 -0400

    agregué funciones para validar nombre, apellido y email dentro de función principal validateForm

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
 function validateForm(){
-	/* Escribe tú código aquí */
-}
+	function name(){
+		var name = document.getElementById("name").value;
+		if(!(/^[A-Z][a-z]{3,19}$/).test(name)){
+			var createSpan= document.createElement("span");
+			var message = document.createTextNode("Nombre inválido, inténtalo otra vez");
+			var fatherClass=document.getElementsByClassName("name-container input-box")[0];
+			createSpan.appendChild(message);
+			fatherClass.appendChild(createSpan);
+		}
+	}
+	name();
+
+	function lastname(){
+		var lastname = document.getElementById("lastname").value;
+		if (!(/^[A-Z][a-z]{3,19}$/).test(apellido)){
+			var createSpan = document.createElement("span");
+			var message = document.createTextNode("Apellido inválido, inténtalo otra vez");
+			var fatherClass = document.getElementsByClassName("lastname-container input-box")[0];
+			createSpan.appendChild(message);
+			fatherClass.appendChild(createSpan);
+		}
+	}
+	lastname();
+
+	function email(){
+		var email = document.getElementById("input-email").value;
+		if(!(/\S+@\S+\.\S+/).test(email));
+	}
+
